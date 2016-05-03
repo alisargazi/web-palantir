61bb28063d4fa810920d19828bebd28658c2aa41
zhanghongbin <122401027@qq.com>
2016-05-03 17:31:42 +0800

    redis组件密码Key 修改

--- utils/redis_util.js
+++ utils/redis_util.js
@@ -5,7 +5,7 @@ var client = redis.createClient({
   host: config.REDIS_HOST,
   port: config.REDIS_PORT,
   db: config.REDIS_DB_INDEX,
-  pass: config.REDIS_DB_PASS  
+  password: config.REDIS_DB_PASS  
 });
 
 module.exports = client;
